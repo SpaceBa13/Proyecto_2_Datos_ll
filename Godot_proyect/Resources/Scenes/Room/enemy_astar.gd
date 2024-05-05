fe3410da9ce60965240d0cb302d3388e7626ad92
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2024-05-05 02:31:37 -0600

    Clean Code

--- Godot_proyect/Resources/Scenes/Room/enemy_astar.gd
+++ Godot_proyect/Resources/Scenes/Room/enemy_astar.gd
@@ -4,9 +4,8 @@ extends CharacterBody2D
 var astar_grid: AStarGrid2D
 @onready var tile_map = $"../DungeonRoom"
 @onready var player = $"../Player"
-var current_point_path: PackedVector2Array
 var current_id_path: Array
-const speed = 1.5
+const speed = 1.3
 
 
 
@@ -14,22 +13,6 @@ func _on_timer_timeout():
 	make_path()
 
 func make_path():
-	astar_grid = AStarGrid2D.new()
-	astar_grid.region = tile_map.get_used_rect()
-	astar_grid.cell_size = Vector2i(16 ,16)
-	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
-	astar_grid.update()
-	
-	for x in tile_map.get_used_rect().size.x:
-		for y in tile_map.get_used_rect().size.y:
-			var tile_position = Vector2i(x + tile_map.get_used_rect().position.x,
-			y + tile_map.get_used_rect().position.y)
-
-			var tile_data = tile_map.get_cell_tile_data(0, tile_position)
-			
-			if tile_data == null or tile_data.get_custom_data("walkable") == false:
-				astar_grid.set_point_solid(tile_position)
-	
 	var own_position = tile_map.local_to_map(global_position)
 	var player_position = tile_map.local_to_map(player.global_position)
 	
@@ -38,8 +21,7 @@ func make_path():
 	
 	if id_path.is_empty() == false:
 		current_id_path = id_path
-		current_point_path = astar_grid.get_point_path(
-			own_position, player_position)
+
 		
 
 
@@ -134,7 +116,6 @@ func get_neighbors(position):
 func is_valid_neighbor(neighbor) -> bool:
 	# Obtener el valor de la capa personalizada "walkable" en la celda vecina
 	var tile_data = tile_map.get_cell_tile_data(0, neighbor)
-
 	if tile_data == null or tile_data.get_custom_data("walkable") == false:
 		return false
 	else:
