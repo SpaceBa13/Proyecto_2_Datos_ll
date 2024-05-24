c376b7700624f3bfcf87ebfb58c69af19365d654
Gustavo Calderon Perez <tuticp04@gmail.com>
2024-05-24 13:58:40 -0600

    cofres terminados

--- Godot_proyect/Scripts/Items/chest.gd
+++ Godot_proyect/Scripts/Items/chest.gd
@@ -12,3 +12,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func actioned():
 	anim.play("open")
+	await get_tree().create_timer(0.6).timeout
+	queue_free()
