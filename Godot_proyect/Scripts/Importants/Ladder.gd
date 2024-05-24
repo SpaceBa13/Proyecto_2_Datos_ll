b1ec4044d26b6c36327485ee1952f77d837cc63b
dargarro@estudiantec.cr <dargarro@estudiantec.cr>
2024-05-24 14:07:25 -0600

    A

--- Godot_proyect/Scripts/Importants/Ladder.gd
+++ Godot_proyect/Scripts/Importants/Ladder.gd
@@ -8,5 +8,7 @@ func _on_body_entered(body:Node2D):
 		var next_level_number = current_scene_file.to_int() + 1
 		
 		var next_level_path = FILE_BEGIN + str(next_level_number) + ".tscn"
+		print(next_level_number)
+		print(current_scene_file)
 		get_tree().change_scene_to_file(next_level_path)
 	
