56748a23c622e6c6ac48a04754e0d68a85b4b341
Gustavo Calderon Perez <tuticp04@gmail.com>
2024-05-24 13:12:57 -0600

    cofre (aun sin terminar :( )

--- Godot_proyect/Scripts/Items/chest.gd
+++ Godot_proyect/Scripts/Items/chest.gd
@@ -2,21 +2,13 @@ extends Node2D
 
 @export var objects: Array[PackedScene]
 @onready var area = $ActionArea
+@onready var anim = $AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	area.actionated.connect(actioned)
 
-# Called when the chest is actioned
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func actioned():
-	# Assuming you want to spawn the first object in the list
-	if objects.size() > 0:
-		var obj_scene = objects[0]
-		var obj_instance = obj_scene.instantiate()
-		add_child(obj_instance)
-		
-		# Calculate the position in front of the chest
-		var spawn_position = global_position + Vector2(50, 0)  # Adjust this vector based on your game
-		obj_instance.global_position = spawn_position
-		
-		print(obj_instance.name)
+	anim.play("open")
