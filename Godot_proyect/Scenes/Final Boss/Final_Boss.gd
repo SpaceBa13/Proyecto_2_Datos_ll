0af66488fee214ab09e897dabddffa3fcb665da9
Brayan Alpizar <130131491+SpaceBa13@users.noreply.github.com>
2024-05-24 13:03:28 -0600

    boss final agregado

--- Godot_proyect/Scenes/Final Boss/Final_Boss.gd
+++ Godot_proyect/Scenes/Final Boss/Final_Boss.gd
@@ -1,28 +1,29 @@
 extends CharacterBody2D
 
 
-class_name Final_Boss
+class_name Gray_Spectre
 
 @onready var tile_map = $"../../DungeonRoom"
 @onready var player = $"../../Player"
 var current_id_path: Array
-const speed = 1.5
+const speed = 0.5
 var spawn_position = Vector2i(0,0)
 var chasing: bool
 @onready var Astar_path = $Astar
 @onready var Backtrack_path = $Backtraking_path 
-@export var maxHealth = 2
-@export var fire_ball: PackedScene
+@export var maxHealth = 15
 @onready var currentHealth: int = maxHealth
 var direction: Vector2
 @onready var animation = $AnimatedSprite2D
+@export var fire_ball: PackedScene
+var death = false
 
 func _ready():
 	spawn_position = tile_map.local_to_map(global_position)
 	chasing = false
 	Astar_path.tile_map = tile_map
 	Backtrack_path.tile_map = tile_map
-	animation.play("default")
+	
 
 func make_astar_path():
 	var own_position = tile_map.local_to_map(global_position)
@@ -38,10 +39,18 @@ func make_astar_path():
 		
 
 func _physics_process(delta):
+	if death == true:
+		animate(direction)
+		queue_free()
+	
+	var own_position_h = tile_map.local_to_map(global_position)
+	var player_position_h = tile_map.local_to_map(player.global_position)
+	var heuristic = Astar_path.heuristic(own_position_h, player_position_h)
+	
 	var tile_data = tile_map.get_cell_tile_data(0, tile_map.local_to_map(player.global_position))
 	if current_id_path.is_empty():
 		if tile_data != null:
-			if tile_data.get_custom_data("safe_zone") != true and player.seen == true:
+			if tile_data.get_custom_data("safe_zone") != true:
 				make_astar_path()
 				return
 			else:
@@ -55,15 +64,16 @@ func _physics_process(delta):
 	#Si la informacion de la celda es diferente de null
 	if tile_data != null:
 		#Obtiene si el jugador esta o no en zona segura
-		if tile_data.get_custom_data("safe_zone") != true and player.seen == true:
-			chasing = true
-			make_astar_path()
-			global_position = global_position.move_toward(target_position, speed)
-			direction = (target_position - global_position).normalized()
-			animate(direction)
-			if global_position.x == target_position.x and global_position.y == target_position.y:
-				print(current_id_path)
-				current_id_path.pop_front()
+		if tile_data.get_custom_data("safe_zone") != true:
+			if heuristic < 25:
+				chasing = true
+				make_astar_path()
+				global_position = global_position.move_toward(target_position, speed)
+				direction = (target_position - global_position).normalized()
+				animate(direction)
+				if global_position.x == target_position.x and global_position.y == target_position.y:
+					print(current_id_path)
+					current_id_path.pop_front()
 				
 		else:
 			if chasing == true:
@@ -89,16 +99,35 @@ func make_backtrack_path(own_position):
 	# Resuelve el laberinto desde la posición inicial
 	current_id_path = Backtrack_path.get_backtrack_path(own_position, spawn_position)
 
-
+func _on_timer_timeout():
+	var tile_data = tile_map.get_cell_tile_data(0, tile_map.local_to_map(player.global_position))
+	if tile_data.get_custom_data("safe_zone") != true:
+		animation.stop()
+		animation.play("Attack")
+		var dir = [Vector2i(1,0), Vector2i(-1,0), Vector2i(0,1), Vector2i(0,-1)]
+		for i in dir:
+			var instance = fire_ball.instantiate()
+			instance.global_position = global_position
+			instance.dir = i
+			add_sibling(instance)
+	
 
 func _on_hitbox_area_entered(area):
 	if area.name == "Swordbox":
 		currentHealth -= 1
 		if currentHealth == 0:
-			queue_free()
+			death = true
 
 
 func animate(direction: Vector2):
+	if direction.x > 0 and death == true:
+		animation.play("Death")
+		animation.flip_h = false
+		animation.rotation = 0
+	if direction.x < 0 and death == true:
+		animation.play("Death")
+		animation.flip_h = false
+		animation.rotation = 0
 	if direction.x > 0:
 			animation.play("default")
 			animation.flip_h = false
@@ -106,3 +135,4 @@ func animate(direction: Vector2):
 	elif direction.x < 0: 
 			animation.play("default")
 			animation.flip_h = true
+	
